9cbfe29fe358fdf0ce44388662701da8780de09d
danyakit <danya.kit26@yandex.ru>
2023-11-23 17:21:19 +0300

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// @todo: Темплейт карточки
+// @todo: Темплейт
 
 // @todo: DOM узлы
 
@@ -7,3 +7,39 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(data, deleteCallback) {
+    const template = document.getElementById('card-template');
+    const clone = document.importNode(template.content, true);
+    const card = clone.querySelector('.card');
+  
+    const cardTitle = card.querySelector('.card__title');
+    const cardImage = card.querySelector('.card__image');
+  
+    cardTitle.textContent = data.name;
+    cardImage.src = data.link;
+  
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function() {
+      deleteCallback(data); 
+      card.remove(); 
+    });
+    return card;
+  }
+  
+  function deleteCard(data) {
+    console.log('Удаление карточки:', data);
+  }
+  
+  function renderInitialCards() {
+    const placesList = document.querySelector('.places__list');
+  
+    initialCards.forEach(function(cardData) {
+      const card = createCard(cardData, deleteCard);
+      placesList.appendChild(card);
+    });
+  }
+  
+  renderInitialCards();
+  
+  
